4ffbc7f2afed07ef6fa3ebc9b4b4b584ba0c422b
prothy <lukacsurgay@gmail.com>
2021-09-28 13:48:25 +0200

    feat (#5, #10): add fields to User entity, Address entity

--- src/main/java/com/codecool/javapeno/erp/entities/Address.java
+++ src/main/java/com/codecool/javapeno/erp/entities/Address.java
@@ -1,4 +1,11 @@
 package com.codecool.javapeno.erp.entities;
 
+import java.util.UUID;
+
 public class Address {
+    private String street;
+    private String city;
+    private int postalCode;
+    private String country;
+    private UUID userId;
 }
--- src/main/java/com/codecool/javapeno/erp/entities/User.java
+++ src/main/java/com/codecool/javapeno/erp/entities/User.java
@@ -1,4 +1,18 @@
 package com.codecool.javapeno.erp.entities;
 
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.UUID;
+
 public class User {
+    private UUID id;
+    private Timestamp createdDate;
+    private Timestamp updatedDate;
+
+    // personal info
+    private String name;
+    private String phoneNumber;
+    private String email;
+    private Address address;
+
 }
