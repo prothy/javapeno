63af7f5893fe560fee42d524cdb73d8b3b85075b
prothy <lukacsurgay@gmail.com>
2021-09-28 13:43:26 +0200

    feat (#2): add fields to Holiday

--- src/main/java/com/codecool/javapeno/erp/entities/Holiday.java
+++ src/main/java/com/codecool/javapeno/erp/entities/Holiday.java
@@ -1,4 +1,11 @@
 package com.codecool.javapeno.erp.entities;
 
+import java.sql.Timestamp;
+import java.util.UUID;
+
 public class Holiday {
+    private UUID id;
+    private Timestamp dateFrom;
+    private Timestamp dateTo;
 }
+
