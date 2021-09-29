b016dfb2f7379da513c7b89f0778754bf4455ffd
Richard Havalda <havalda.r94@gmail.com>
2021-09-29 10:30:51 +0200

    feat (#7): initialize UserService, implement addNewUser method

--- src/main/java/com/codecool/javapeno/erp/services/UserService.java
+++ src/main/java/com/codecool/javapeno/erp/services/UserService.java
@@ -1,4 +1,21 @@
 package com.codecool.javapeno.erp.services;
 
+import com.codecool.javapeno.erp.entities.User;
+import com.codecool.javapeno.erp.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public void addNewUser(User user) {
+        userRepository.save(user);
+    }
 }
