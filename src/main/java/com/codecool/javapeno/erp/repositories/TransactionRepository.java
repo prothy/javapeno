427136cdf0e611731a1eb082f164068d13631f21
Ichy91 <farago.istvan91@gmail.com>
2021-09-29 15:35:45 +0200

    feat (#13): add find all by month, year and user id query select method

--- src/main/java/com/codecool/javapeno/erp/repositories/TransactionRepository.java
+++ src/main/java/com/codecool/javapeno/erp/repositories/TransactionRepository.java
@@ -3,6 +3,7 @@ package com.codecool.javapeno.erp.repositories;
 import com.codecool.javapeno.erp.entities.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,6 +12,9 @@ import java.util.UUID;
 @Repository("transactionRepository")
 public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
 
-    @Query("select t from Transaction t where year(t.timestamp) = ?1 and month(t.timestamp) = ?2")
-    List<Transaction> findAllByTimestamp(Integer month, Integer year);
+    @Query("select t from Transaction t where year(t.timestamp) = :year and month(t.timestamp) = :month")
+    List<Transaction> findAllByTimestamp(@Param("month") Integer month, @Param("year") Integer year);
+
+    @Query("select t from Transaction t where year(t.timestamp) = :year and month(t.timestamp) = :month and t.userId = :userId")
+    List<Transaction> findAllByTimestampAndUserId(@Param("month") Integer month, @Param("year") Integer year, @Param("userId") UUID userId);
 }
