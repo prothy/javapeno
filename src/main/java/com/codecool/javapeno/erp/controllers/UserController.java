b93de9b216b6b024ed09a508809d7d978540018c
Zoltán Baranyay <zbaranyay@gmail.com>
2021-09-29 12:11:32 +0200

    feat (#11): create getUser() method for the modify-user/{id} route
    This route return a selected user

--- src/main/java/com/codecool/javapeno/erp/controllers/UserController.java
+++ src/main/java/com/codecool/javapeno/erp/controllers/UserController.java
@@ -1,4 +1,27 @@
 package com.codecool.javapeno.erp.controllers;
 
+import com.codecool.javapeno.erp.models.UserModel;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
 public class UserController {
+
+    /**
+     *
+     * @param id user id
+     * @return the selected user data
+     */
+    @GetMapping("/modify-user/{id}")
+    public ResponseEntity<Object> getUser(@PathVariable UUID id) {
+        UserModel userModel = new UserModel();
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(userModel);
+    }
 }
