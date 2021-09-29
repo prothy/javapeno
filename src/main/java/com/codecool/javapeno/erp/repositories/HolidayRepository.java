13afafc896436f07b0b46d81a84e7bb33ccd6e2c
prothy <lukacsurgay@gmail.com>
2021-09-29 14:43:45 +0200

    feat (#2): add filter by user id and date range

--- src/main/java/com/codecool/javapeno/erp/repositories/HolidayRepository.java
+++ src/main/java/com/codecool/javapeno/erp/repositories/HolidayRepository.java
@@ -5,10 +5,12 @@ import com.codecool.javapeno.erp.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
 @Repository("holidayRepository")
 public interface HolidayRepository extends JpaRepository<Holiday, UUID> {
     List<Holiday> findAllByUserId(UUID id);
+    List<Holiday> findAllByUserAndAndDateFromAndDateTo(UUID id, Date dateFrom, Date dateTo);
 }
