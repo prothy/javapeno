85e8e076dcc0b4e2b7cce5a75b047c7a0143a748
Ichy91 <farago.istvan91@gmail.com>
2021-09-29 13:44:21 +0200

    feat (#12): set up the TransactionController

--- src/main/java/com/codecool/javapeno/erp/controllers/TransactionController.java
+++ src/main/java/com/codecool/javapeno/erp/controllers/TransactionController.java
@@ -1,4 +1,18 @@
 package com.codecool.javapeno.erp.controllers;
 
+import com.codecool.javapeno.erp.services.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/transaction-service")
 public class TransactionController {
+
+    private final TransactionService transactionService;
+
+    @Autowired
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
 }
