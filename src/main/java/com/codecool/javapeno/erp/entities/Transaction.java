539d39f8acc52afb2cc663b17d5b2fd829c4bb44
Ichy91 <farago.istvan91@gmail.com>
2021-09-29 14:39:52 +0200

    feat (#12): set up the Transaction entity

--- src/main/java/com/codecool/javapeno/erp/entities/Transaction.java
+++ src/main/java/com/codecool/javapeno/erp/entities/Transaction.java
@@ -1,13 +1,30 @@
 package com.codecool.javapeno.erp.entities;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.sql.Timestamp;
 import java.util.UUID;
 
+@Entity
+@Table(name = "transactions")
 public class Transaction {
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name="system-uuid", strategy = "uuid")
     private UUID id;
+
+    @NotNull
     private Timestamp timestamp;
+
+    @NotNull
     private BigDecimal amount;
+
+    @NotNull
     private String accountNumFrom;
+
+    @NotNull
     private String accountNumTo;
 }
