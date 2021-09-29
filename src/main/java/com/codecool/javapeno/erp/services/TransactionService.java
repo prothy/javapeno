7c8aa7e91b28c8b95d56520d491470ed15895a8e
Ichy91 <farago.istvan91@gmail.com>
2021-09-29 14:01:34 +0200

    feat (#12): add method for get all transactions by month and year

--- src/main/java/com/codecool/javapeno/erp/services/TransactionService.java
+++ src/main/java/com/codecool/javapeno/erp/services/TransactionService.java
@@ -1,9 +1,12 @@
 package com.codecool.javapeno.erp.services;
 
+import com.codecool.javapeno.erp.entities.Transaction;
 import com.codecool.javapeno.erp.repositories.TransactionRepository;
 import com.codecool.javapeno.erp.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TransactionService {
@@ -15,5 +18,9 @@ public class TransactionService {
     public TransactionService(TransactionRepository transactionRepository, UserRepository userRepository) {
         this.transactionRepository = transactionRepository;
         this.userRepository = userRepository;
+    }
+
+    public List<Transaction> getTransactionsByMonthAndYear(int month, int year) {
+        return null;
     }
 }
