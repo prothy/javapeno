0a103ab8240ee9472f50ba3394124760a9a8c1e8
prothy <lukacsurgay@gmail.com>
2021-09-29 11:12:05 +0200

    feat (#23): add User and Address entities so it connects to db

--- src/main/java/com/codecool/javapeno/erp/entities/User.java
+++ src/main/java/com/codecool/javapeno/erp/entities/User.java
@@ -1,11 +1,21 @@
 package com.codecool.javapeno.erp.entities;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.UUID;
 
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name="system-uuid", strategy = "uuid")
     private UUID id;
+
     private LocalDate createdDate;
     private LocalDate updatedDate;
 
@@ -13,11 +23,13 @@ public class User {
     private String name;
     private String phoneNumber;
     private String email;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "address_id", referencedColumnName = "id")
     private Address address;
 
     // work info
     private UserStatus status;
     private UserPrivilege privilege;
     private BigDecimal salary;
-
 }
