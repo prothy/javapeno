403489d8c9eba2ec58d751223a98f46d2564b7c3
VepeP <57757745+VepeP@users.noreply.github.com>
2021-09-29 11:20:47 +0200

    feat (#4): implement TransactionService

--- src/main/java/com/codecool/javapeno/erp/services/TransactionService.java
+++ src/main/java/com/codecool/javapeno/erp/services/TransactionService.java
@@ -1,4 +1,24 @@
 package com.codecool.javapeno.erp.services;
 
+import com.codecool.javapeno.erp.models.UserTransactionModel;
+import com.codecool.javapeno.erp.repositories.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
 public class TransactionService {
+
+    private final TransactionRepository transactionRepository;
+
+    @Autowired
+    public TransactionService(TransactionRepository transactionRepository) {
+        this.transactionRepository = transactionRepository;
+    }
+
+    public UserTransactionModel getUserTransactionById(UUID id) {
+        return transactionRepository.findAllUserTransactionById(id);
+    }
+
 }
