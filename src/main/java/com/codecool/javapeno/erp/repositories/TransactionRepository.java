3f065404088495e3fb9541ec87b16b43a18e104f
Ichy91 <farago.istvan91@gmail.com>
2021-09-29 15:05:02 +0200

    feat (#12): add query select for all transaction by month and year

--- src/main/java/com/codecool/javapeno/erp/repositories/TransactionRepository.java
+++ src/main/java/com/codecool/javapeno/erp/repositories/TransactionRepository.java
@@ -2,10 +2,15 @@ package com.codecool.javapeno.erp.repositories;
 
 import com.codecool.javapeno.erp.entities.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository("transactionRepository")
 public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
+
+    @Query("select t from Transaction t where year(t.timestamp) = ?1 and month(t.timestamp) = ?2")
+    List<Transaction> findAllByTimestamp(Integer month, Integer year);
 }
