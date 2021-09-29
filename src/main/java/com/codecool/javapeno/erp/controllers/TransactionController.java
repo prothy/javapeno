c2695271691202b7c556c0c94861e3a6b82a7faa
VepeP <57757745+VepeP@users.noreply.github.com>
2021-09-29 11:20:29 +0200

    feat (#4): implement TransactionController

--- src/main/java/com/codecool/javapeno/erp/controllers/TransactionController.java
+++ src/main/java/com/codecool/javapeno/erp/controllers/TransactionController.java
@@ -1,4 +1,28 @@
 package com.codecool.javapeno.erp.controllers;
 
+import com.codecool.javapeno.erp.models.UserTransactionModel;
+import com.codecool.javapeno.erp.services.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("api/transaction-service")
 public class TransactionController {
+
+    private final TransactionService transactionService;
+
+    @Autowired
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+    @GetMapping("/get-user-transaction/{id}")
+    public UserTransactionModel getUserTransaction(@PathVariable UUID id) {
+        return transactionService.getUserTransactionById(id);
+    }
 }
