c766444344625b53d4e016a2496db99a83cab803
Ichy91 <farago.istvan91@gmail.com>
2021-09-29 13:44:36 +0200

    feat (#12): set up the TransactionService

--- src/main/java/com/codecool/javapeno/erp/services/TransactionService.java
+++ src/main/java/com/codecool/javapeno/erp/services/TransactionService.java
@@ -1,4 +1,19 @@
 package com.codecool.javapeno.erp.services;
 
+import com.codecool.javapeno.erp.repositories.TransactionRepository;
+import com.codecool.javapeno.erp.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class TransactionService {
+
+    private final TransactionRepository transactionRepository;
+    private final UserRepository userRepository;
+
+    @Autowired
+    public TransactionService(TransactionRepository transactionRepository, UserRepository userRepository) {
+        this.transactionRepository = transactionRepository;
+        this.userRepository = userRepository;
+    }
 }
