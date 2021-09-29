befbbbdf265622ca5143e272deca882c0614f30e
Zoltán Baranyay <zbaranyay@gmail.com>
2021-09-29 19:38:41 +0200

    feat (#11): implement the modifyUserById() method in the UserController class
    Change the getUser() method and the /modify-user/{id} route to the modifyUserById() and /user/{id} route

--- src/main/java/com/codecool/javapeno/erp/controllers/UserController.java
+++ src/main/java/com/codecool/javapeno/erp/controllers/UserController.java
@@ -1,21 +1,10 @@
 package com.codecool.javapeno.erp.controllers;
 
-import com.codecool.javapeno.erp.models.UserModel;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.codecool.javapeno.erp.entities.User;
 import com.codecool.javapeno.erp.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.UUID;
-
 
 import java.util.UUID;
 
@@ -40,16 +29,8 @@ public class UserController {
         return userService.getUserById(id);
     }
 
-    /**
-     *
-     * @param id user id
-     * @return the selected user data
-     */
-    @GetMapping("/modify-user/{id}")
-    public ResponseEntity<Object> getUser(@PathVariable UUID id) {
-        UserModel userModel = new UserModel();
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(userModel);
+    @PostMapping("/user/{id}")
+    public ResponseEntity<String> modifyUserById(@PathVariable UUID id, @RequestBody User updatedUserData) {
+        return userService.updateUserById(id, updatedUserData);
     }
 }
