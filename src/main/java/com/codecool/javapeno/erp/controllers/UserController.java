db51fa0abe4445ca3f035a1804b8f6d0b1fc488e
Richard Havalda <havalda.r94@gmail.com>
2021-09-29 10:28:38 +0200

    feat (#7): add restcontroller annotation, created register method

--- src/main/java/com/codecool/javapeno/erp/controllers/UserController.java
+++ src/main/java/com/codecool/javapeno/erp/controllers/UserController.java
@@ -1,4 +1,26 @@
 package com.codecool.javapeno.erp.controllers;
 
+import com.codecool.javapeno.erp.entities.User;
+import com.codecool.javapeno.erp.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/user")
 public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public void addNewUser(@RequestBody User user) {
+        userService.addNewUser(user);
+    }
 }
