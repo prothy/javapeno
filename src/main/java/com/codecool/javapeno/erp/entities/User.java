48d47cdab77d54e2156b75c9e9934fe69da5cacc
Ichy91 <farago.istvan91@gmail.com>
2021-09-29 11:04:36 +0200

    feat (#10): add getters and setters for the User

--- src/main/java/com/codecool/javapeno/erp/entities/User.java
+++ src/main/java/com/codecool/javapeno/erp/entities/User.java
@@ -49,4 +49,75 @@ public class User {
     @Enumerated(EnumType.STRING)
     private BigDecimal salary;
 
+    public UUID getId() {
+        return id;
+    }
+
+    public LocalDate getCreatedDate() {
+        return createdDate;
+    }
+
+    public LocalDate getUpdatedDate() {
+        return updatedDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public UserStatus getStatus() {
+        return status;
+    }
+
+    public UserPrivilege getPrivilege() {
+        return privilege;
+    }
+
+    public BigDecimal getSalary() {
+        return salary;
+    }
+
+    public void setUpdatedDate(LocalDate updatedDate) {
+        this.updatedDate = updatedDate;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public void setStatus(UserStatus status) {
+        this.status = status;
+    }
+
+    public void setPrivilege(UserPrivilege privilege) {
+        this.privilege = privilege;
+    }
+
+    public void setSalary(BigDecimal salary) {
+        this.salary = salary;
+    }
 }
