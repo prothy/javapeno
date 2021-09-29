8b02da4749478933085e1d20e03d1e38afe2538b
Ichy91 <farago.istvan91@gmail.com>
2021-09-29 10:38:08 +0200

    feat (#10): add method for find the user by id

--- src/main/java/com/codecool/javapeno/erp/services/UserService.java
+++ src/main/java/com/codecool/javapeno/erp/services/UserService.java
@@ -1,8 +1,12 @@
 package com.codecool.javapeno.erp.services;
 
+import com.codecool.javapeno.erp.entities.User;
 import com.codecool.javapeno.erp.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.UUID;
 
 @Service
 public class UserService {
@@ -11,5 +15,9 @@ public class UserService {
     @Autowired
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    private User getUserById(UUID userId) {
+        return (User) userRepository.findAllById(Collections.singleton(userId));
     }
 }
