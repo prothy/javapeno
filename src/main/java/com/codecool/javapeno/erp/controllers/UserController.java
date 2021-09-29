6dab8fb96a392ac26432d59e1e48ac83e63dced0
Ichy91 <farago.istvan91@gmail.com>
2021-09-29 10:26:35 +0200

    feat (#10): set up the UserController.

--- src/main/java/com/codecool/javapeno/erp/controllers/UserController.java
+++ src/main/java/com/codecool/javapeno/erp/controllers/UserController.java
@@ -1,4 +1,18 @@
 package com.codecool.javapeno.erp.controllers;
 
+import com.codecool.javapeno.erp.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/user-service")
 public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 }
