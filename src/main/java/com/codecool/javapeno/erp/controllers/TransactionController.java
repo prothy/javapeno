975479d915792adde70846b21789ebfc15e53087
Ichy91 <farago.istvan91@gmail.com>
2021-09-29 13:58:35 +0200

    feat (#12): add endpoint for the monthly report

--- src/main/java/com/codecool/javapeno/erp/controllers/TransactionController.java
+++ src/main/java/com/codecool/javapeno/erp/controllers/TransactionController.java
@@ -1,9 +1,14 @@
 package com.codecool.javapeno.erp.controllers;
 
+import com.codecool.javapeno.erp.entities.Transaction;
 import com.codecool.javapeno.erp.services.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/transaction-service")
@@ -14,5 +19,10 @@ public class TransactionController {
     @Autowired
     public TransactionController(TransactionService transactionService) {
         this.transactionService = transactionService;
+    }
+
+    @GetMapping("/monthly-report/{month}/{year}")
+    public List<Transaction> getMonthlyReport(@PathVariable int month, @PathVariable int year) {
+        return null;
     }
 }
