265177c57c71f331b51537afaeef99dfc1615718
Ichy91 <farago.istvan91@gmail.com>
2021-09-29 10:27:35 +0200

    feat (#10): add end point for the user data by id

--- src/main/java/com/codecool/javapeno/erp/controllers/UserController.java
+++ src/main/java/com/codecool/javapeno/erp/controllers/UserController.java
@@ -1,9 +1,14 @@
 package com.codecool.javapeno.erp.controllers;
 
+import com.codecool.javapeno.erp.entities.User;
 import com.codecool.javapeno.erp.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("api/user-service")
@@ -14,5 +19,10 @@ public class UserController {
     @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
+    }
+
+    @GetMapping("/user/{id}")
+    public User getUserById(@PathVariable UUID id) {
+        return null;
     }
 }
