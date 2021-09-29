7e859f8182f3f294f6dcddc7340846fa0a3ee171
Zoltán Baranyay <zbaranyay@gmail.com>
2021-09-29 20:01:17 +0200

    feat (#10): implement getAddressById() method in the AddressService class

--- src/main/java/com/codecool/javapeno/erp/services/AddressService.java
+++ src/main/java/com/codecool/javapeno/erp/services/AddressService.java
@@ -1,8 +1,14 @@
 package com.codecool.javapeno.erp.services;
 
+import com.codecool.javapeno.erp.entities.Address;
 import com.codecool.javapeno.erp.repositories.AddressRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class AddressService {
@@ -11,6 +17,18 @@ public class AddressService {
     @Autowired
     public AddressService(AddressRepository addressRepository) {
         this.addressRepository = addressRepository;
+    }
+
+    public ResponseEntity<Object> getAddressById(UUID addressId) {
+        Optional<Address> address = addressRepository.findById(addressId);
+        if (address.isEmpty()) {
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .body("Address is not found");
+        }
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(address);
     }
 
 }
