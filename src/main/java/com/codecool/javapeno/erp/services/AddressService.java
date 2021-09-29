dc18add70de184a4242e09c3d424df669a731bb4
Zoltán Baranyay <zbaranyay@gmail.com>
2021-09-29 19:57:14 +0200

    feat (#10): create the AddressService class

---
+++ src/main/java/com/codecool/javapeno/erp/services/AddressService.java
@@ -0,0 +1,16 @@
+package com.codecool.javapeno.erp.services;
+
+import com.codecool.javapeno.erp.repositories.AddressRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AddressService {
+    private final AddressRepository addressRepository;
+
+    @Autowired
+    public AddressService(AddressRepository addressRepository) {
+        this.addressRepository = addressRepository;
+    }
+
+}
