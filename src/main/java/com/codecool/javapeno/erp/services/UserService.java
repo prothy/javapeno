3cc17684956280aa69cf498cf9e62041cf470bba
Ichy91 <farago.istvan91@gmail.com>
2021-09-29 10:29:32 +0200

    feat (#10): set up the UserService

--- src/main/java/com/codecool/javapeno/erp/services/UserService.java
+++ src/main/java/com/codecool/javapeno/erp/services/UserService.java
@@ -1,4 +1,15 @@
 package com.codecool.javapeno.erp.services;
 
+import com.codecool.javapeno.erp.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 }
