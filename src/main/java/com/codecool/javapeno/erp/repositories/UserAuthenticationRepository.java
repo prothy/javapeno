7413a0cda1b9c54da89bdfa7b149369ed07deb34
Richard Havalda <havalda.r94@gmail.com>
2021-09-30 10:36:26 +0200

    feat (#7): add new repository for authentication

---
+++ src/main/java/com/codecool/javapeno/erp/repositories/UserAuthenticationRepository.java
@@ -0,0 +1,6 @@
+package com.codecool.javapeno.erp.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserAuthenticationRepository extends JpaRepository {
+}
