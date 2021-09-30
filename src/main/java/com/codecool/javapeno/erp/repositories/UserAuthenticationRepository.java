468f96becd90364a88fe2eb4812a3a102e445f8a
Richard Havalda <havalda.r94@gmail.com>
2021-09-30 10:44:49 +0200

    fix (#7): raw type fixed

--- src/main/java/com/codecool/javapeno/erp/repositories/UserAuthenticationRepository.java
+++ src/main/java/com/codecool/javapeno/erp/repositories/UserAuthenticationRepository.java
@@ -1,6 +1,9 @@
 package com.codecool.javapeno.erp.repositories;
 
+import com.codecool.javapeno.erp.entities.UserAuthentication;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserAuthenticationRepository extends JpaRepository {
+import java.util.UUID;
+
+public interface UserAuthenticationRepository extends JpaRepository<UserAuthentication, UUID> {
 }
