2c475992d0a18f34153f63c98f3d96c6e1cf36c7
Ichy91 <farago.istvan91@gmail.com>
2021-09-30 16:02:17 +0200

    feat (#38): add ui endpoint documentation

--- src/main/java/com/codecool/javapeno/erp/ErpApplication.java
+++ src/main/java/com/codecool/javapeno/erp/ErpApplication.java
@@ -2,8 +2,10 @@ package com.codecool.javapeno.erp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class ErpApplication {
 
 	public static void main(String[] args) {
