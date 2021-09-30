d469c0540062bc953d2a80d1313d4e642ee9e3bf
prothy <lukacsurgay@gmail.com>
2021-09-30 10:51:07 +0200

    feat (#2): add service methods to connecto HolidayRepository and holiday controllers in UserController

--- src/main/java/com/codecool/javapeno/erp/services/UserService.java
+++ src/main/java/com/codecool/javapeno/erp/services/UserService.java
@@ -1,23 +1,38 @@
 package com.codecool.javapeno.erp.services;
 
+import com.codecool.javapeno.erp.entities.Holiday;
 import com.codecool.javapeno.erp.entities.User;
+import com.codecool.javapeno.erp.repositories.HolidayRepository;
 import com.codecool.javapeno.erp.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.Collections;
+import java.util.List;
 import java.util.UUID;
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final HolidayRepository holidayRepository;
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, HolidayRepository holidayRepository) {
         this.userRepository = userRepository;
+        this.holidayRepository = holidayRepository;
     }
 
     public User getUserById(UUID userId) {
         return (User) userRepository.findAllById(Collections.singleton(userId));
+    }
+
+    public List<Holiday> getAllHolidaysById(UUID userId) {
+        return holidayRepository.findAllByUserId(userId);
+    }
+
+    public List<Holiday> getAllHolidaysByIdBetweenRange(UUID userId, LocalDate from, LocalDate to) {
+        return holidayRepository.findAllByUserIdAndDateFromAndDateTo(userId, from, to);
     }
 }
