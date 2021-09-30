b90d9d875c7d9b722f93e4f609cc61759c938289
prothy <lukacsurgay@gmail.com>
2021-09-30 10:50:23 +0200

    fix (#2): change date to localdate in HolidayRepository to ensure compatibility with controller

--- src/main/java/com/codecool/javapeno/erp/repositories/HolidayRepository.java
+++ src/main/java/com/codecool/javapeno/erp/repositories/HolidayRepository.java
@@ -5,12 +5,12 @@ import com.codecool.javapeno.erp.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Date;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
 @Repository("holidayRepository")
 public interface HolidayRepository extends JpaRepository<Holiday, UUID> {
     List<Holiday> findAllByUserId(UUID id);
-    List<Holiday> findAllByUserAndAndDateFromAndDateTo(UUID id, Date dateFrom, Date dateTo);
+    List<Holiday> findAllByUserIdAndDateFromAndDateTo(UUID user_id, LocalDate dateFrom, LocalDate dateTo);
 }
