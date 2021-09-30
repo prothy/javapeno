d22dbb6514ebbffd7b10d82083b06a605e803665
Ichy91 <farago.istvan91@gmail.com>
2021-09-30 11:24:12 +0200

    refact: add annotation to the end points

--- src/main/java/com/codecool/javapeno/erp/controllers/TransactionController.java
+++ src/main/java/com/codecool/javapeno/erp/controllers/TransactionController.java
@@ -4,12 +4,8 @@ import com.codecool.javapeno.erp.entities.Transaction;
 import com.codecool.javapeno.erp.models.UserTransactionModel;
 import com.codecool.javapeno.erp.services.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 import java.util.UUID;
 
@@ -25,23 +21,17 @@ public class TransactionController {
     }
 
     @GetMapping("/get-user-transactions")
-    public List<UserTransactionModel> getUserTransaction(HttpServletRequest request) {
-        String stringId = request.getParameter("id");
+    public List<UserTransactionModel> getUserTransaction(@RequestParam(value = "userId", required = false) UUID userId) {
+        if (userId == null) return null;
 
-        if (transactionService.isIdNull(stringId)) return null;
-
-        UUID id = UUID.fromString(stringId);
-        return transactionService.getUserTransactionsById(id);
+        return transactionService.getUserTransactionsById(userId);
     }
 
     @GetMapping("/get-users-top-transaction")
-    public UserTransactionModel getUsersTopTransaction(HttpServletRequest request) {
-        String stringId = request.getParameter("id");
+    public UserTransactionModel getUsersTopTransaction(@RequestParam(value = "userId", required = false) UUID userId) {
+        if (userId == null) return null;
 
-        if (transactionService.isIdNull(stringId)) return null;
-
-        UUID id = UUID.fromString(stringId);
-        return transactionService.getUsersTopTransactionsById(id);
+        return transactionService.getUsersTopTransactionsById(userId);
     }
 
     @GetMapping("/monthly-report/{month}/{year}")
