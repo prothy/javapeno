f53f22761d2143934c0e993b4bbe8f5b2501518d
Zoltán Baranyay <zbaranyay@gmail.com>
2021-09-30 17:24:18 +0200

    refactor (#36): add a slash to the TransactionController route

--- src/main/java/com/codecool/javapeno/erp/controllers/TransactionController.java
+++ src/main/java/com/codecool/javapeno/erp/controllers/TransactionController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("api/transaction")
+@RequestMapping("/api/transaction")
 public class TransactionController {
 
     private final TransactionService transactionService;
