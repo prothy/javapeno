6e14843e0c5b3d31fe4d7d806aa82953776a27b5
Richard Havalda <havalda.r94@gmail.com>
2021-10-12 09:24:06 +0200

    fix (#16) remove div in App.js

--- javapeno-erp/src/App.js
+++ javapeno-erp/src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
 import Javapeno from './containers/Javapeno';
 import Layout from './components/Layout/Layout';
+import Aux from './hoc/Aux';
 
 function App() {
   return (
-    <div>
+    <Aux>
       <Layout>
         <Javapeno />
       </Layout>
-    </div>
+    </Aux>
   );
 }
 
