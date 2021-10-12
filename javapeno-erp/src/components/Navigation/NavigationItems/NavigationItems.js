fcd9ced8f66421ec7a758daa75305df0f8a04c74
Richard Havalda <havalda.r94@gmail.com>
2021-10-12 08:46:55 +0200

    feat (#16} add navigation items component

---
+++ javapeno-erp/src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import classes from './NavigationItems.css';
+
+const navigationItems = () => <ul className={classes.NavigationItems}></ul>;
+
+export default navigationItems;
