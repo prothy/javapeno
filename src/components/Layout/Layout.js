936bc930dc543fcb4c5c6e8ab8e2b173ee87c330
Zoltán Baranyay <zbaranyay@gmail.com>
2021-10-12 13:27:53 +0200

    feat (#2): add grid container to the Layout.js file

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,14 +2,30 @@ import React from 'react';
 import Footer from '../Navigation/Footer/Footer';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import {Col, Container, Row} from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Layout = (props) => {
   return (
     <>
-      <Toolbar />
-      <SideDrawer />
-      <main>{props.children}</main>
-      <Footer />
+        <Toolbar />
+        <Container fluid>
+            <Row>
+                <Col xs={2}>
+                    <p>Test</p>
+                    <p>Test</p>
+                    <p>Test</p>
+                </Col>
+                <Col xs={10}>
+                    <p>Test</p>
+                    <p>Test</p>
+                    <p>Test</p>
+                </Col>
+            </Row>
+        </Container>
+        <SideDrawer />
+        <main>{props.children}</main>
+        <Footer />
     </>
   );
 };
