afef5d7818987fc4c01149afe25e306c63d28c3f
prothy <lukacsurgay@gmail.com>
2021-10-12 14:42:10 +0200

    feat: add CORS header config bean to main class

--- src/main/java/com/codecool/javapeno/erp/ErpApplication.java
+++ src/main/java/com/codecool/javapeno/erp/ErpApplication.java
@@ -2,6 +2,10 @@ package com.codecool.javapeno.erp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -13,5 +17,19 @@ public class ErpApplication {
 	}
 
 	//http://localhost:8080/swagger-ui.html
+
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:3000")
+						.allowCredentials(true)
+						.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+			}
+		};
+	}
 
 }
