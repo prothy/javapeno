cca695e824b0b01f88bfb9f67297ddc8a022c364
Zoltán Baranyay <zbaranyay@gmail.com>
2021-10-12 15:08:49 +0200

    feat (#2): create container for the sidebar and the content container

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Footer from '../Navigation/Footer/Footer';
-import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import {Col, Container, Row} from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -11,20 +11,16 @@ const Layout = (props) => {
         <Toolbar />
         <Container fluid>
             <Row>
-                <Col xs={2}>
-                    <p>Test</p>
-                    <p>Test</p>
-                    <p>Test</p>
+                <Col xs={2} className={"d-flex flex-column flex-shrink-0 p-3"}>
+                    <SideDrawer/>
                 </Col>
                 <Col xs={10}>
-                    <p>Test</p>
+                    <main>{props.children}</main>
                     <p>Test</p>
                     <p>Test</p>
                 </Col>
             </Row>
         </Container>
-        <SideDrawer />
-        <main>{props.children}</main>
         <Footer />
     </>
   );
