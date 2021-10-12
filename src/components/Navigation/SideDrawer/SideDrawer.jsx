c2e25eff487ac5ad82ffdf6ee81bde8b7cf460b3
Zoltán Baranyay <zbaranyay@gmail.com>
2021-10-12 13:41:02 +0200

    refactor (#2): rename SideDrawer.js to SideDrawer.jsx

--- src/components/Navigation/SideDrawer/SideDrawer.jsx
+++ src/components/Navigation/SideDrawer/SideDrawer.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 //import classes from './SideDrawer.module.css';
 
 const SideDrawer = () => {
-  return <aside>SIDEDRAWER</aside>;
+  return(
+      <aside>
+        <a href={"#"}>Employee</a>
+        <a href={"#"}>Transaction</a>
+      </aside>);
 };
 
 export default SideDrawer;
