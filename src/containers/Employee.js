20ae9d95c056dc48573cd81e20c83cd5cf77e98a
VepeP <57757745+VepeP@users.noreply.github.com>
2021-10-12 14:25:27 +0200

    feat (#6): WIP fetch data from backend

--- src/containers/Employee.js
+++ src/containers/Employee.js
@@ -1,21 +1,30 @@
-/* User data
-private UUID id;
-private LocalDate updatedDate;
-private String name;
-private String phoneNumber;
-private String email;
-private Address address;
-private UserStatus status;
-private UserPrivilege privilege;
-private BigDecimal salary;
-*/
-
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Table from 'react-bootstrap/Table';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from "./Header";
 import Body from "./Body";
 
 let Employee = () => {
+    /*
+    const [items, setData] = useState([]);
+
+    // Similar to componentDidMount and componentDidUpdate:
+    try{
+        useEffect(() => {fetch(
+            "http://localhost:8080/api/user/8cb3a14a-e68e-f902-badb-3e9877e6b330", {
+                method: 'GET',
+                credentials: 'same-origin'
+            })
+            .then((res) => res.json())
+            .then((json) => {
+                setData(json);
+            })
+        });
+    }catch (e) {
+        console.log("No senor fetch nem lenni")
+    }
+    */
+
     let header = ['Name', 'Phone number', 'E-mail', 'Address', 'Salary']
     let body = [['Name', 'Phone number', 'E-mail', 'Address', 'Salary']]
     return (
