88e951c08a4fc77fbe3c2f52530b8da829d37a3d
Zoltán Baranyay <zbaranyay@gmail.com>
2021-10-12 15:10:20 +0200

    feat (#2): create links in the SideDrawer.jsx

--- src/components/Navigation/SideDrawer/SideDrawer.jsx
+++ src/components/Navigation/SideDrawer/SideDrawer.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
+import {Link} from "react-router-dom";
+import {CashStack, House, Person} from "react-bootstrap-icons";
 //import classes from './SideDrawer.module.css';
 
 const SideDrawer = () => {
   return(
-      <aside>
-        <a href={"#"}>Employee</a>
-        <a href={"#"}>Transaction</a>
-      </aside>);
+      <ul className={"nav nav-pills flex-column mb-auto"}>
+          <li className={"nav-item"}>
+              <Link className={"nav-link link-dark"} to={"/"}>
+                    <House/>
+                  Home
+              </Link>
+          </li>
+          <li className={"nav-item"}>
+              <Link className={"nav-link active"} to={"/employees"}>
+                  <Person/>
+                  Employees
+              </Link>
+          </li>
+          <li className={"nav-item"}>
+              <Link className={"nav-link link-dark"}  to={"/transactions"}>
+                  <CashStack/>
+                  Transactions
+              </Link>
+          </li>
+      </ul>
+
+      );
 };
 
 export default SideDrawer;
