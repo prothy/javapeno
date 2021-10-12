5982b2cc8ede9c2bba76e9515b37177dbd866cfa
VepeP <57757745+VepeP@users.noreply.github.com>
2021-10-12 13:35:31 +0200

    feat (#6): call employee component

--- src/containers/Javapeno.js
+++ src/containers/Javapeno.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import classes from './Javapeno.module.css';
+import Employee from "./Employee";
 
 const Javapeno = () => {
-  return (
-    <>
-      <h1 className={classes.Javapeno}>CONTENT</h1>
-    </>
-  );
+    return (
+        <>
+            <h1 className={classes.Javapeno}>CONTENT</h1>
+            <Employee/>
+        </>
+    );
 };
 
 export default Javapeno;
