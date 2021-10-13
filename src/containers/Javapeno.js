72f4e3c5c21a786de50daecc750409a5db1c5add
VepeP <57757745+VepeP@users.noreply.github.com>
2021-10-13 13:18:03 +0200

    refactor (#6): refactor file structure

--- src/containers/Javapeno.js
+++ src/containers/Javapeno.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import classes from './Javapeno.module.css';
-import Employee from "./Employee";
 
 const Javapeno = () => {
     return (
         <>
             <h1 className={classes.Javapeno}>CONTENT</h1>
-            <Employee/>
         </>
     );
 };
