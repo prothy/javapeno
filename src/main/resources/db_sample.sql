74b2d1c0391d8ea41691ee2675ffdb98db98874a
VepeP <57757745+VepeP@users.noreply.github.com>
2021-10-14 22:51:04 +0200

    refactor: modify db_sample.sql with new users

--- src/main/resources/db_sample.sql
+++ src/main/resources/db_sample.sql
@@ -9,13 +9,95 @@ values (uuid_in((md5('xyz3f59d-94a4-5f67-5b43-8e3a73434dc6'))::cstring), 'Warsza
 
 
 insert into users (id, created_date, email, name, phone_number, privilege, salary, status, updated_date, address_id)
-values (uuid_in((md5('8cb3a14a-e68e-f902-badb-3e9877e6b330'))::cstring), now(), 'test@codecool.com', 'Teszt Elek', '+3611234567', 'USER', 320000, 'ACTIVE', now(), uuid_in((md5('bce3f59d-94a4-5f67-5b43-8e3a73434dc6'))::cstring));
+values (uuid_in((md5('8cb3a14a-e68e-f902-badb-3e9877e6b330'))::cstring), date(current_timestamp), 'test@codecool.com', 'Teszt Elek', '+3611234567', 'USER', 320000, 'ACTIVE', date(current_timestamp), uuid_in((md5('bce3f59d-94a4-5f67-5b43-8e3a73434dc6'))::cstring));
 
 insert into users (id, created_date, email, name, phone_number, privilege, salary, status, updated_date, address_id)
-values (uuid_in((md5('28b483cc-f11f-ad50-0743-996de7cb01c4'))::cstring), now(), 'tester@codecool.com', 'Minta Aladár', '+3619876543', 'USER', 300000, 'ACTIVE', now(), uuid_in((md5('abc3f59d-94a4-5f67-5b43-8e3a73434dc6'))::cstring));
+values (uuid_in((md5('28b483cc-f11f-ad50-0743-996de7cb01c4'))::cstring), date(current_timestamp), 'tester@codecool.com', 'Minta Aladár', '+3619876543', 'USER', 300000, 'ACTIVE', date(current_timestamp), uuid_in((md5('abc3f59d-94a4-5f67-5b43-8e3a73434dc6'))::cstring));
 
 insert into users (id, created_date, email, name, phone_number, privilege, salary, status, updated_date, address_id)
-values (uuid_in((md5('xrz3f59d-94a4-6d89-5b43-8e3a73434dc6'))::cstring), now(), 'programmer@codecool.com', 'Git Áron', '+3614658443', 'USER', 280000, 'ACTIVE', now(), uuid_in((md5('xyz3f59d-94a4-5f67-5b43-8e3a73434dc6'))::cstring));
+values (uuid_in((md5('xrz3f59d-94a4-6d89-5b43-8e3a73434dc6'))::cstring), date(current_timestamp), 'programmer@codecool.com', 'Git Áron', '+3614658443', 'USER', 280000, 'ACTIVE', date(current_timestamp), uuid_in((md5('xyz3f59d-94a4-5f67-5b43-8e3a73434dc6'))::cstring));
+
+
+
+insert into users (id, created_date, email, name, phone_number, privilege, salary, status, updated_date, address_id)
+values (uuid_in(md5(random()::text || clock_timestamp()::text)::cstring), date(current_timestamp), 'programmer1@codecool.com', 'Tra Pista', null, 'USER', 380000, 'ACTIVE', date(current_timestamp), null);
+
+
+insert into users (id, created_date, email, name, phone_number, privilege, salary, status, updated_date, address_id)
+values (uuid_in(md5(random()::text || clock_timestamp()::text)::cstring), date(current_timestamp), 'programmer2@codecool.com', 'Tatum Hoover', null, 'USER', 380000, 'ACTIVE', date(current_timestamp), null);
+
+
+insert into users (id, created_date, email, name, phone_number, privilege, salary, status, updated_date, address_id)
+values (uuid_in(md5(random()::text || clock_timestamp()::text)::cstring), date(current_timestamp), 'programmer3@codecool.com', 'Rayan Woodard', null, 'USER', 380000, 'ACTIVE', date(current_timestamp), null);
+
+
+insert into users (id, created_date, email, name, phone_number, privilege, salary, status, updated_date, address_id)
+values (uuid_in(md5(random()::text || clock_timestamp()::text)::cstring), date(current_timestamp), 'programmer4@codecool.com', 'Leo Sullivan', null, 'USER', 380000, 'ACTIVE', date(current_timestamp), null);
+
+
+insert into users (id, created_date, email, name, phone_number, privilege, salary, status, updated_date, address_id)
+values (uuid_in(md5(random()::text || clock_timestamp()::text)::cstring), date(current_timestamp), 'programmer5@codecool.com', 'Jeffrey Silva', null, 'USER', 380000, 'ACTIVE', date(current_timestamp), null);
+
+
+insert into users (id, created_date, email, name, phone_number, privilege, salary, status, updated_date, address_id)
+values (uuid_in(md5(random()::text || clock_timestamp()::text)::cstring), date(current_timestamp), 'programmer6@codecool.com', 'Kellen Haley', null, 'USER', 380000, 'ACTIVE', date(current_timestamp), null);
+
+
+insert into users (id, created_date, email, name, phone_number, privilege, salary, status, updated_date, address_id)
+values (uuid_in(md5(random()::text || clock_timestamp()::text)::cstring), date(current_timestamp), 'programmer7@codecool.com', 'Marcos Craig', null, 'USER', 380000, 'ACTIVE', date(current_timestamp), null);
+
+
+insert into users (id, created_date, email, name, phone_number, privilege, salary, status, updated_date, address_id)
+values (uuid_in(md5(random()::text || clock_timestamp()::text)::cstring), date(current_timestamp), 'programmer8@codecool.com', 'Tiffany Ray', null, 'USER', 380000, 'ACTIVE', date(current_timestamp), null);
+
+
+insert into users (id, created_date, email, name, phone_number, privilege, salary, status, updated_date, address_id)
+values (uuid_in(md5(random()::text || clock_timestamp()::text)::cstring), date(current_timestamp), 'programmer9@codecool.com', 'Sawyer Underwood', null, 'USER', 380000, 'ACTIVE', date(current_timestamp), null);
+
+
+insert into users (id, created_date, email, name, phone_number, privilege, salary, status, updated_date, address_id)
+values (uuid_in(md5(random()::text || clock_timestamp()::text)::cstring), date(current_timestamp), 'programmer10@codecool.com', 'Lena Keller', null, 'USER', 380000, 'ACTIVE', date(current_timestamp), null);
+
+
+insert into users (id, created_date, email, name, phone_number, privilege, salary, status, updated_date, address_id)
+values (uuid_in(md5(random()::text || clock_timestamp()::text)::cstring), date(current_timestamp), 'programmer11@codecool.com', 'Hector Patrick', null, 'USER', 380000, 'ACTIVE', date(current_timestamp), null);
+
+
+insert into users (id, created_date, email, name, phone_number, privilege, salary, status, updated_date, address_id)
+values (uuid_in(md5(random()::text || clock_timestamp()::text)::cstring), date(current_timestamp), 'programmer12@codecool.com', 'Michael Oxlong', null, 'USER', 380000, 'ACTIVE', date(current_timestamp), null);
+
+
+insert into users (id, created_date, email, name, phone_number, privilege, salary, status, updated_date, address_id)
+values (uuid_in(md5(random()::text || clock_timestamp()::text)::cstring), date(current_timestamp), 'programmer13@codecool.com', 'Isabelle Oconnor', null, 'USER', 380000, 'ACTIVE', date(current_timestamp), null);
+
+
+insert into users (id, created_date, email, name, phone_number, privilege, salary, status, updated_date, address_id)
+values (uuid_in(md5(random()::text || clock_timestamp()::text)::cstring), date(current_timestamp), 'programmer14@codecool.com', 'Nap Pali', null, 'USER', 380000, 'ACTIVE', date(current_timestamp), null);
+
+
+insert into users (id, created_date, email, name, phone_number, privilege, salary, status, updated_date, address_id)
+values (uuid_in(md5(random()::text || clock_timestamp()::text)::cstring), date(current_timestamp), 'programmer15@codecool.com', 'Evie Wood', null, 'USER', 380000, 'ACTIVE', date(current_timestamp), null);
+
+
+insert into users (id, created_date, email, name, phone_number, privilege, salary, status, updated_date, address_id)
+values (uuid_in(md5(random()::text || clock_timestamp()::text)::cstring), date(current_timestamp), 'programmer16@codecool.com', 'Byron Carey', null, 'USER', 380000, 'ACTIVE', date(current_timestamp), null);
+
+
+insert into users (id, created_date, email, name, phone_number, privilege, salary, status, updated_date, address_id)
+values (uuid_in(md5(random()::text || clock_timestamp()::text)::cstring), date(current_timestamp), 'programmer17@codecool.com', 'Maximilian Foster', null, 'USER', 380000, 'ACTIVE', date(current_timestamp), null);
+
+
+insert into users (id, created_date, email, name, phone_number, privilege, salary, status, updated_date, address_id)
+values (uuid_in(md5(random()::text || clock_timestamp()::text)::cstring), date(current_timestamp), 'programmer18@codecool.com', 'Rex Middleton', null, 'USER', 380000, 'ACTIVE', date(current_timestamp), null);
+
+
+insert into users (id, created_date, email, name, phone_number, privilege, salary, status, updated_date, address_id)
+values (uuid_in(md5(random()::text || clock_timestamp()::text)::cstring), date(current_timestamp), 'programmer19@codecool.com', 'Lawrence Nunez', null, 'USER', 380000, 'ACTIVE', date(current_timestamp), null);
+
+
+insert into users (id, created_date, email, name, phone_number, privilege, salary, status, updated_date, address_id)
+values (uuid_in(md5(random()::text || clock_timestamp()::text)::cstring), date(current_timestamp), 'programmer20@codecool.com', 'Maximilian Foster', null, 'USER', 380000, 'ACTIVE', date(current_timestamp), null);
+
 
 
 insert into transactions (id, account_num_from, account_num_to, amount, "timestamp", user_id)
