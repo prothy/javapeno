26bfb569b2d97de40403a4208fa7ec2ab3ccd0cc
Ichy91 <farago.istvan91@gmail.com>
2021-10-14 13:46:54 +0200

    feat (#38): add swagger documentation to the User entity

--- src/main/java/com/codecool/javapeno/erp/entities/User.java
+++ src/main/java/com/codecool/javapeno/erp/entities/User.java
@@ -3,6 +3,9 @@ package com.codecool.javapeno.erp.entities;
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
@@ -12,53 +15,58 @@ import java.sql.Timestamp;
 import java.util.List;
 import java.util.UUID;
 
+@ApiModel(description = "Details about the user")
 @Entity
 @Table(name = "users")
 public class User {
-    @Column
+
+    @ApiModelProperty(notes = "The unique id of the user")
     @Id
     @GeneratedValue
-    @GenericGenerator(name="postgres-uuid", strategy = "uuid")
+    @GenericGenerator(name = "postgres-uuid", strategy = "uuid")
     private UUID id;
 
+    @ApiModelProperty(notes = "The day when created the user")
     @CreationTimestamp
     @Column(nullable = false, updatable = false)
     private Timestamp createdDate;
 
+    @ApiModelProperty(notes = "The last day when modify some user data")
     @UpdateTimestamp
-    @Column
     private Timestamp updatedDate;
 
     // personal info
-    @Column
+    @ApiModelProperty(notes = "The person's name")
     @NotNull
     private String name;
 
-    @Column
+    @ApiModelProperty(notes = "The person's phone number")
     private String phoneNumber;
 
-    @Column
+    @ApiModelProperty(notes = "The person's email address")
     @Email(message = "Email should be valid")
     @NotNull
     private String email;
 
+    @ApiModelProperty(notes = "The person's address")
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "address_id", referencedColumnName = "id")
     private Address address;
 
     // work info
-    @Column
+    @ApiModelProperty(notes = "The user activity status")
     @NotNull
     @Enumerated(EnumType.STRING)
     private UserStatus status;
 
-    @Column
+    @ApiModelProperty(notes = "The user privilege")
     @NotNull
     @Enumerated(EnumType.STRING)
     private UserPrivilege privilege;
 
-    @Column
+    @ApiModelProperty(notes = "The user monthly salary")
     private BigDecimal salary;
+
 
     public void setId(UUID id) {
         this.id = id;
