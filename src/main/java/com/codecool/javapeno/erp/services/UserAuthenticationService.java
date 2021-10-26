cfabf33e74dc080ab2858f5acd0e2eb6e156b266
Ichy91 <farago.istvan91@gmail.com>
2021-10-26 14:16:02 +0200

    feat (#48): add loadUserByUsername method from the UserDetailsService interface

--- src/main/java/com/codecool/javapeno/erp/services/UserAuthenticationService.java
+++ src/main/java/com/codecool/javapeno/erp/services/UserAuthenticationService.java
@@ -2,17 +2,22 @@ package com.codecool.javapeno.erp.services;
 
 import com.codecool.javapeno.erp.entities.UserAuthentication;
 import com.codecool.javapeno.erp.repositories.UserAuthenticationRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserAuthenticationService {
-
+@RequiredArgsConstructor
+public class UserAuthenticationService implements UserDetailsService {
     private final UserAuthenticationRepository userAuthenticationRepository;
 
-    @Autowired
-    public UserAuthenticationService(UserAuthenticationRepository userAuthenticationRepository) {
-        this.userAuthenticationRepository = userAuthenticationRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//        UserAuthentication userAuthentication =
+        return null;
     }
 
     public void registerAuthentication(UserAuthentication userAuthentication) {
