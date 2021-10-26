ca1eab3ee509a4c8fae76923c5da48e8dbb90636
Zoltán Baranyay <zbaranyay@gmail.com>
2021-10-26 13:16:24 +0200

    refactor (#33): remove unused import statements

--- src/main/java/com/codecool/javapeno/erp/controllers/UserController.java
+++ src/main/java/com/codecool/javapeno/erp/controllers/UserController.java
@@ -5,8 +5,6 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.codecool.javapeno.erp.entities.User;
 import com.codecool.javapeno.erp.services.UserService;
