13c6db5aec7bc8d5d6040d087da56e34dbff2c36
VepeP <57757745+VepeP@users.noreply.github.com>
2021-10-27 16:19:03 +0200

    feat (#41): make change password component visible

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Switch } from 'react-router';
+import {Route, Switch} from 'react-router';
 
 import './App.css';
 
@@ -10,32 +10,32 @@ import Transactions from "./components/Transactions/Transactions";
 import EmployeeForm from "./components/Employee/EmployeeForm";
 
 function App() {
-  return (
-    <>
-      <Layout>
-        <Switch>
-          <Route path="/employee/:userId">
-            <Employee/>
-          </Route>
-          <Route path="/create-employee" >
-            <EmployeeForm />
-          </Route>
-          <Route path="/employees">
-            <EmployeeList />
-          </Route>
-          <Route path="/transactions">
-            <Transactions />
-          </Route>
-          <Route path="/home" >
-            <Javapeno />
-          </Route>
-          <Route path="/" >
-            <Javapeno />
-          </Route>
-        </Switch>
-      </Layout>
-    </>
-  );
+    return (
+        <>
+            <Layout>
+                <Switch>
+                    <Route path="/employee/:userId">
+                        <Employee/>
+                    </Route>
+                    <Route path="/create-employee">
+                        <EmployeeForm/>
+                    </Route>
+                    <Route path="/employees">
+                        <EmployeeList/>
+                    </Route>
+                    <Route path="/transactions">
+                        <Transactions/>
+                    </Route>
+                    <Route path="/home">
+                        <Javapeno/>
+                    </Route>
+                    <Route path="/">
+                        <Javapeno/>
+                    </Route>
+                </Switch>
+            </Layout>
+        </>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
-
+import {BrowserRouter as Router} from 'react-router-dom';
+import {Route, Switch} from 'react-router';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ChangePassword from "./components/Employee/ChangePassword";
+import Header from "./components/Navigation/Header/Header";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Router>
+            <Switch>
+                <Route path="/user/change-password/:userId">
+                    <Header/>
+                    <ChangePassword/>
+                </Route>
+                <Route path="/">
+                    <App/>
+                </Route>
+            </Switch>
+        </Router>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
