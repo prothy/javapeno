7f413c33a80d3a4e935588f5fe2e462a552396c4
prothy <lukacsurgay@gmail.com>
2021-10-27 12:16:46 +0200

    feat (#15): complete coverage of UserService

--- src/test/java/com/codecool/javapeno/erp/services/UserServiceUnitTest.java
+++ src/test/java/com/codecool/javapeno/erp/services/UserServiceUnitTest.java
@@ -1,10 +1,12 @@
 package com.codecool.javapeno.erp.services;
 
+import com.codecool.javapeno.erp.entities.Holiday;
 import com.codecool.javapeno.erp.entities.User;
 import com.codecool.javapeno.erp.repositories.HolidayRepository;
 import com.codecool.javapeno.erp.repositories.UserRepository;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
@@ -20,6 +22,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
@@ -42,49 +45,103 @@ class UserServiceUnitTest {
     @InjectMocks
     private UserService userService;
 
-    UUID uuid;
+    private UUID uuid;
+    private User user;
 
     @BeforeEach
     void initTest() {
         uuid = UUID.randomUUID();
+        user = new User();
     }
 
-    @Test
-    void getUserById_returnsUserIfExistsInDb() {
-        User initUser = new User();
-        when(userRepository.findById(uuid)).thenReturn(Optional.of(initUser));
+    @Nested
+    class Positive {
+        @BeforeEach
+        void initPositiveTest() {
+            when(userRepository.findById(uuid)).thenReturn(Optional.of(user));
+        }
 
-        User foundUser = userService.getUserById(uuid);
 
-        assertEquals(foundUser, initUser);
+        @Test
+        void getUserById_returnsUserIfExistsInDb() {
+            User foundUser = userService.getUserById(uuid);
+
+            assertEquals(foundUser, user);
+        }
+
+        @Test
+        void updateUserById_returnsValueIfSuccessful() {
+            when(userRepository.save(user)).thenReturn(user);
+
+            assertNotNull(userService.updateUserById(uuid, user));
+            assertDoesNotThrow(() -> userService.updateUserById(uuid, user));
+        }
+
+        @Test
+        void deactivateUser_returnsValueIfSuccessful() {
+            assertNotNull(userService.deactivateUser(uuid));
+            assertDoesNotThrow(() -> userService.deactivateUser(uuid));
+        }
+
+        @Test
+        void getHolidaysByIdInRange_returnsValueIfSuccessful() {
+            LocalDate from = LocalDate.of(1970, 1, 1);
+            LocalDate to = LocalDate.now();
+
+            assertNotNull(userService.getHolidaysByIdInRange(uuid, from, to));
+            assertDoesNotThrow(() -> userService.getHolidaysByIdInRange(uuid, from, to));
+
+            assertNotNull(userService.getHolidaysByIdInRange(uuid, from, null));
+            assertDoesNotThrow(() -> userService.getHolidaysByIdInRange(uuid, from, null));
+
+            assertNotNull(userService.getHolidaysByIdInRange(uuid, null, to));
+            assertDoesNotThrow(() -> userService.getHolidaysByIdInRange(uuid, null, to));
+        }
+
+        @Test
+        void addHolidayToUser_returnsValueIfSuccessful() {
+            Holiday holiday = new Holiday();
+
+            assertNotNull(userService.addHolidayToUser(uuid, holiday));
+            assertDoesNotThrow(() -> userService.addHolidayToUser(uuid, holiday));
+        }
     }
 
-    @Test
-    void getUserById_throwsExceptionIfNotFoundInDb() {
-        when(userRepository.findById(uuid)).thenReturn(Optional.empty());
+    @Nested
+    class Negative {
+        @BeforeEach
+        void initNegativeTest() {
+            when(userRepository.findById(uuid)).thenReturn(Optional.empty());
+        }
 
-        assertThrows(NoSuchElementException.class, () -> userService.getUserById(uuid));
-    }
+        @Test
+        void getUserById_throwsExceptionIfNotFoundInDb() {
+            assertThrows(NoSuchElementException.class, () -> userService.getUserById(uuid));
+        }
 
-    @Test
-    void updateUserById_returnsValueIfSuccessful() {
-        User user = new User();
-        when(userRepository.findById(uuid)).thenReturn(Optional.of(user));
-        when(userRepository.save(user)).thenReturn(user);
+        @Test
+        void updateUserById_throwsExceptionIfNotFoundInDb() {
+            assertThrows(NoSuchElementException.class, () -> userService.updateUserById(uuid, user));
+        }
 
-        assertNotNull(userService.updateUserById(uuid, user));
-        assertDoesNotThrow(() -> userService.updateUserById(uuid, user));
-    }
+        @Test
+        void deactivateUser_throwsExceptionIfNotFoundInDb() {
+            assertThrows(NoSuchElementException.class, () -> userService.deactivateUser(uuid));
+        }
 
-    @Test
-    void updateUserById_throwsExceptionIfNotFoundInDb() {
-        User user = new User();
-        when(userRepository.findById(uuid)).thenReturn(Optional.empty());
+        @Test
+        void getHolidaysByIdInRange_throwsExceptionIfNotFoundInDb() {
+            LocalDate from = LocalDate.of(1970, 1, 1);
+            LocalDate to = LocalDate.now();
 
-        assertThrows(NoSuchElementException.class, () -> userService.updateUserById(uuid, user));
-    }
+            assertThrows(NoSuchElementException.class, () -> userService.getHolidaysByIdInRange(uuid, from, to));
+        }
 
-    @Test
-    void deactivateUser_returnsValueIfSuccessful() {
+        @Test
+        void addHolidayToUser_throwsExceptionIfNotFoundInDb() {
+            Holiday holiday = new Holiday();
+
+            assertThrows(NoSuchElementException.class, () -> userService.addHolidayToUser(uuid, holiday));
+        }
     }
 }
