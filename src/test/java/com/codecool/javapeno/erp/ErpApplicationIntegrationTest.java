9cd567c0f807466bf0341e702a2a2b420ce97c07
prothy <lukacsurgay@gmail.com>
2021-10-27 09:41:27 +0200

    feat (#15): add test to check if spring application context loads correctly

---
+++ src/test/java/com/codecool/javapeno/erp/ErpApplicationIntegrationTest.java
@@ -0,0 +1,18 @@
+package com.codecool.javapeno.erp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class ErpApplicationIntegrationTest {
+
+	@Test
+	void contextLoads(ApplicationContext context) {
+		assertThat(context).isNotNull();
+	}
+
+}
