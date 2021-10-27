f5653135f0beea8c5cd3356deb567b6fc52e06fb
VepeP <57757745+VepeP@users.noreply.github.com>
2021-10-27 14:34:44 +0200

    feat (#49): implement email service and logic

--- src/main/java/com/codecool/javapeno/erp/services/EmailSenderService.java
+++ src/main/java/com/codecool/javapeno/erp/services/EmailSenderService.java
@@ -1,10 +1,21 @@
 package com.codecool.javapeno.erp.services;
 
+import com.codecool.javapeno.erp.dto.MailResponse;
 import com.codecool.javapeno.erp.entities.User;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
 
 @Service
 public class EmailSenderService {
@@ -12,23 +23,36 @@ public class EmailSenderService {
     @Autowired
     private JavaMailSender mailSender;
 
-    public void sendEmail(User user) {
+    @Autowired
+    private Configuration config;
 
-        String toEmail = user.getEmail();
-        String subject = "First login to the website";
-        String body = "Dear " + user.getName() + "!\n" +
-                "Welcome to the company, please click on the following link, and change your generated password.\n\n" +
-                "[link]\n\n" +
-                "Thanks you!\n" +
-                "IT team";
-        SimpleMailMessage message = new SimpleMailMessage();
+    public void sendEmail(User user, Map<String, Object> model) {
+        MailResponse response = new MailResponse();
+        MimeMessage message = mailSender.createMimeMessage();
+        try {
+            String subject = "Welcome to Javapeño";
+            MimeMessageHelper helper = new MimeMessageHelper(message, MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
+                    StandardCharsets.UTF_8.name());
 
-        message.setFrom("javapeno2021@gmail.com");
-        message.setTo(toEmail);
-        message.setText(body);
-        message.setSubject(subject);
+            Template t = config.getTemplate("email-template.ftl");
+            String html = FreeMarkerTemplateUtils.processTemplateIntoString(t, model);
 
-        mailSender.send(message);
+            helper.setFrom("javapeno2021@gmail.com");
+            helper.setTo(user.getEmail());
+            helper.setSubject(subject);
+            helper.setText(html, true);
+
+            mailSender.send(message);
+            System.out.println("email sent");
+            response.setMessage("mail send to : " + user.getEmail());
+            response.setStatus(Boolean.TRUE);
+
+        } catch (MessagingException | IOException | TemplateException e) {
+            response.setMessage("Mail Sending failure : "+e.getMessage());
+            response.setStatus(Boolean.FALSE);
+        }
+
+        //return response;
     }
 
 }
