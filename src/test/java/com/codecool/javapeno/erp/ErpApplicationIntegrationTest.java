4fa8dae91290ac6d64c6e90b5f1c80f9b914f540
prothy <lukacsurgay@gmail.com>
2021-10-27 10:19:07 +0200

    refactor (#15): remove unused dependency add comment to remind to set environment variables for run configuration

--- src/test/java/com/codecool/javapeno/erp/ErpApplicationIntegrationTest.java
+++ src/test/java/com/codecool/javapeno/erp/ErpApplicationIntegrationTest.java
@@ -1,7 +1,6 @@
 package com.codecool.javapeno.erp;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 
@@ -10,6 +9,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 class ErpApplicationIntegrationTest {
 
+	// if context does not load check if environment variables are set for test configs
 	@Test
 	void contextLoads(ApplicationContext context) {
 		assertThat(context).isNotNull();
