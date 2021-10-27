ae49497eccb1196ea4c6b7b322bae3f2eddf5c8d
Zoltán Baranyay <zbaranyay@gmail.com>
2021-10-27 13:13:14 +0200

    feat (#55): add exception handling in the TransactionController class

--- src/main/java/com/codecool/javapeno/erp/controllers/TransactionController.java
+++ src/main/java/com/codecool/javapeno/erp/controllers/TransactionController.java
@@ -1,22 +1,32 @@
 package com.codecool.javapeno.erp.controllers;
 
 import com.codecool.javapeno.erp.entities.Transaction;
+import com.codecool.javapeno.erp.models.ErrorModel;
 import com.codecool.javapeno.erp.models.UserTransactionModel;
 import com.codecool.javapeno.erp.services.TransactionService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/transaction")
-@RequiredArgsConstructor
 public class TransactionController {
     private final TransactionService transactionService;
+    private final ErrorModel errorModel;
+
+    @Autowired
+    public TransactionController(TransactionService transactionService, ErrorModel errorModel) {
+        this.transactionService = transactionService;
+        this.errorModel = errorModel;
+    }
 
     @GetMapping("/all")
     public Page<UserTransactionModel> getUserTransactions(@RequestParam(value = "userId", required = false) UUID userId, Pageable pageable) {
@@ -44,5 +54,21 @@ public class TransactionController {
 
 
         return transactionService.getAllTransactionsByUserBetweenDates(userId, dateFrom, dateTo);
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ErrorModel notValidUUID() {
+        errorModel.setErrorMessage("Not valid id");
+        errorModel.setTime(LocalDateTime.now());
+        errorModel.setStatus(HttpStatus.NOT_FOUND);
+        return errorModel;
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ErrorModel userNotFound(Exception exception) {
+        errorModel.setErrorMessage(exception.getMessage());
+        errorModel.setTime(LocalDateTime.now());
+        errorModel.setStatus(HttpStatus.NOT_FOUND);
+        return errorModel;
     }
 }
