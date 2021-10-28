a50b8b5d888016e4169e6d3c70917a4869459af9
VepeP <57757745+VepeP@users.noreply.github.com>
2021-10-28 13:10:19 +0200

    feat (#49): implement password change for user

--- src/main/java/com/codecool/javapeno/erp/controllers/UserAuthenticationController.java
+++ src/main/java/com/codecool/javapeno/erp/controllers/UserAuthenticationController.java
@@ -1,9 +1,11 @@
 package com.codecool.javapeno.erp.controllers;
 
 import com.codecool.javapeno.erp.entities.UserAuthentication;
+import com.codecool.javapeno.erp.models.PasswordChangeModel;
 import com.codecool.javapeno.erp.services.UserAuthenticationService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,5 +21,15 @@ public class UserAuthenticationController {
     @PostMapping(path = "/register")
     public void registerAuthentication(@RequestBody UserAuthentication userAuthentication) {
         userAuthenticationService.registerAuthentication(userAuthentication);
+    }
+
+    @PostMapping("/first-password-change")
+    @ApiOperation(
+            value = "Change users password for the first time",
+            notes = "Change users password for the first time")
+    public void userFirstPasswordChange(
+            @ApiParam(value = "Users email and new password", required = true)
+            @RequestBody PasswordChangeModel passwordChangeModel) {
+        userAuthenticationService.changeUsersPassword(passwordChangeModel);
     }
 }
