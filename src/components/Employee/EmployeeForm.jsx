3d78b837bd066cc8fad295d0c70672c73164baab
Zoltán Baranyay <zbaranyay@gmail.com>
2021-10-28 20:21:54 +0200

    feat (#11): add user data if its exist to the form and add more country to the country selector

--- src/components/Employee/EmployeeForm.jsx
+++ src/components/Employee/EmployeeForm.jsx
@@ -86,14 +86,14 @@ const EmployeeForm = (props, callback, deps) => {
                         <td>
                             <FormGroup className={"mb-3"} controlId={"name"}>
                                 <FormLabel>Full name</FormLabel>
-                                <FormControl type={"text"} placeholder={"John Doe"}
+                                <FormControl type={"text"} value={value.name} placeholder={"John Doe"}
                                              onChange={(event => setValue({...value, name: event.target.value}))}/>
                             </FormGroup>
                         </td>
                         <td>
                             <FormGroup className={"mb-3"} controlId={"phoneNumber"}>
                                 <FormLabel>Phone number</FormLabel>
-                                <FormControl type={"tel"} placeholder={"+36101234567"}
+                                <FormControl type={"tel"} value={value.phoneNumber} placeholder={"+36101234567"}
                                              onChange={(event => setValue({
                                                  ...value,
                                                  phoneNumber: event.target.value
@@ -103,7 +103,7 @@ const EmployeeForm = (props, callback, deps) => {
                         <td>
                             <FormGroup className={"mb-3"} controlId={"email"}>
                                 <FormLabel>Email</FormLabel>
-                                <FormControl type={"email"} placeholder={"john@doe.com"}
+                                <FormControl type={"email"} value={value.email} placeholder={"john@doe.com"}
                                              onChange={(event => setValue({...value, email: event.target.value}))}/>
                             </FormGroup>
                         </td>
@@ -115,30 +115,33 @@ const EmployeeForm = (props, callback, deps) => {
                         <td>
                             <FormGroup className={"mb-3"} controlId={"country"}>
                                 <FormLabel>Country</FormLabel>
-                                <Form.Select onChange={(event => setValue({...value, country: event.target.value}))}>
-                                    <option>Hungary</option>
-                                    <option>Austria</option>
-                                    <option>Slovakia</option>
-                                    <option>Ukraine</option>
-                                    <option>Romania</option>
-                                    <option>Serbia</option>
-                                    <option>Croatia</option>
-                                    <option>Slovenia</option>
-                                    <option>Wakanda</option>
+                                <Form.Select defaultValue={value.country} onChange={(event => setValue({...value, country: event.target.value}))}>
+                                    <option unselectable={true}>Choose a country...</option>
+                                    <option value={"hungary"}>Hungary</option>
+                                    <option value={"germany"}>Germany</option>
+                                    <option value={"french"}>French</option>
+                                    <option value={"austria"}>Austria</option>
+                                    <option value={"slovakia"}>Slovakia</option>
+                                    <option value={"ukraine"}>Ukraine</option>
+                                    <option value={"romania"}>Romania</option>
+                                    <option value={"serbia"}>Serbia</option>
+                                    <option value={"croatia"}>Croatia</option>
+                                    <option value={"slovenia"}>Slovenia</option>
+                                    <option value={"poland"}>Poland</option>
                                 </Form.Select>
                             </FormGroup>
                         </td>
                         <td>
                             <FormGroup className={"mb-3"} controlId={"city"}>
                                 <FormLabel>City</FormLabel>
-                                <FormControl type={"text"} placeholder={"Budapest"}
+                                <FormControl type={"text"} value={value.city} placeholder={"Budapest"}
                                              onChange={(event => setValue({...value, city: event.target.value}))}/>
                             </FormGroup>
                         </td>
                         <td>
                             <FormGroup className={"mb-3"} controlId={"postalCode"}>
                                 <FormLabel>Zip</FormLabel>
-                                <FormControl type={"text"} placeholder={"1234"}
+                                <FormControl type={"text"} value={value.postalCode} placeholder={"1234"}
                                              onChange={(event => setValue({
                                                  ...value,
                                                  postalCode: event.target.value
@@ -150,14 +153,14 @@ const EmployeeForm = (props, callback, deps) => {
                         <td colSpan={2}>
                             <FormGroup className={"mb-3"} controlId={"street"}>
                                 <FormLabel>Address</FormLabel>
-                                <FormControl type={"text"} placeholder={"Example street"}
+                                <FormControl type={"text"} value={value.street} placeholder={"Example street"}
                                              onChange={(event => setValue({...value, street: event.target.value}))}/>
                             </FormGroup>
                         </td>
                         <td>
                             <FormGroup className={"mb-3"} controlId={"houseNumber"}>
                                 <FormLabel>House number</FormLabel>
-                                <FormControl type={"text"} placeholder={"1"}
+                                <FormControl type={"text"} value={value.houseNumber} placeholder={"1"}
                                              onChange={(event => setValue({
                                                  ...value,
                                                  houseNumber: event.target.value
@@ -172,28 +175,30 @@ const EmployeeForm = (props, callback, deps) => {
                         <td>
                             <FormGroup className={"mb-3"} controlId={"salary"}>
                                 <FormLabel>Salary</FormLabel>
-                                <FormControl type={"number"} placeholder={"10000"}
+                                <FormControl type={"number"} value={value.salary} placeholder={"10000"}
                                              onChange={(event => setValue({...value, salary: event.target.value}))}/>
                             </FormGroup>
                         </td>
                         <td>
                             <FormGroup className={"mb-3"} controlId={"status"}>
                                 <FormLabel>Status</FormLabel>
-                                <Form.Select onChange={(event => setValue({...value, status: event.target.value}))}>
-                                    <option>ACTIVE</option>
-                                    <option>DELETED</option>
-                                    <option>HOLIDAY</option>
-                                    <option>SICK</option>
+                                <Form.Select defaultValue={value.status} onChange={(event => setValue({...value, status: event.target.value}))}>
+                                    <option unselectable={true}>Choose a status...</option>
+                                    <option value={"ACTIVE"}>Active</option>
+                                    <option value={"DELETED"}>Deleted</option>
+                                    <option value={"HOLIDAY"}>Holiday</option>
+                                    <option value={"SICK"}>Sick</option>
                                 </Form.Select>
                             </FormGroup>
                         </td>
                         <td>
                             <FormGroup className={"mb-3"} controlId={"privilege"}>
                                 <FormLabel>Privilege</FormLabel>
-                                <Form.Select onChange={(event => setValue({...value, privilege: event.target.value}))}>
-                                    <option>USER</option>
-                                    <option>SUPER_USER</option>
-                                    <option>ADMIN</option>
+                                <Form.Select defaultValue={value.privilege} onChange={(event => setValue({...value, privilege: event.target.value}))}>
+                                    <option unselectable={true}>Choose a privilege...</option>
+                                    <option value={"USER"}>User</option>
+                                    <option value={"SUPER_USER"}>Super user</option>
+                                    <option value={"ADMIN"}>Admin</option>
                                 </Form.Select>
                             </FormGroup>
                         </td>
@@ -209,93 +214,6 @@ const EmployeeForm = (props, callback, deps) => {
                 </Table>
             </Form>
         </div>
-        <Form onSubmitCapture={onSubmitHandler} style={{marginBottom: "100px"}}>
-            <FormGroup className={"mb-3"} controlId={"name"} >
-                <FormLabel>Full name</FormLabel>
-                <FormControl type={"text"}  placeholder={"John Doe"} value={value.name} onChange={(event => setValue({...value, name: event.target.value}))} />
-            </FormGroup>
-            <FormGroup className={"mb-3"} controlId={"phoneNumber"} >
-                <FormLabel>Phone number</FormLabel>
-                <FormControl type={"tel"} placeholder={"+36101234567"} value={value.phoneNumber} onChange={(event => setValue({...value, phoneNumber: event.target.value}))}/>
-            </FormGroup>
-            <FormGroup className={"mb-3"} controlId={"email"}>
-                <FormLabel>Email</FormLabel>
-                <FormControl type={"email"} placeholder={"john@doe.com"} value={value.email} onChange={(event => setValue({...value, email: event.target.value}))}/>
-            </FormGroup>
-            <Row>
-                <Col >
-                    <FormGroup className={"mb-3"} controlId={"country"}>
-                        <FormLabel>Country</FormLabel>
-                        <Form.Select defaultValue={value.country} onChange={(event => setValue({...value, country: event.target.value}))}>
-                            <option unselectable={true}>Choose a country...</option>
-                            <option value={"hungary"}>Hungary</option>
-                            <option value={"germany"}>Germany</option>
-                            <option value={"french"}>French</option>
-                            <option value={"austria"}>Austria</option>
-                            <option value={"slovakia"}>Slovakia</option>
-                            <option value={"ukraine"}>Ukraine</option>
-                            <option value={"romania"}>Romania</option>
-                            <option value={"serbia"}>Serbia</option>
-                            <option value={"croatia"}>Croatia</option>
-                            <option value={"slovenia"}>Slovenia</option>
-                            <option value={"poland"}>Poland</option>
-                        </Form.Select>
-                    </FormGroup>
-                </Col>
-                <Col>
-                    <FormGroup className={"mb-3"} controlId={"city"}>
-                        <FormLabel>City</FormLabel>
-                        <FormControl type={"text"} placeholder={"Budapest"} value={value.city} onChange={(event => setValue({...value, city: event.target.value}))}/>
-                    </FormGroup>
-                </Col>
-                <Col>
-                    <FormGroup className={"mb-3"} controlId={"postalCode"}>
-                        <FormLabel>Zip</FormLabel>
-                        <FormControl type={"text"} placeholder={"1234"} value={value.postalCode} onChange={(event => setValue({...value, postalCode: event.target.value}))}/>
-                    </FormGroup>
-                </Col>
-            </Row>
-            <Row>
-                <Col xs={8}>
-                    <FormGroup className={"mb-3"} controlId={"street"}>
-                        <FormLabel>Address</FormLabel>
-                        <FormControl type={"text"} placeholder={"Example street"} value={value.street} onChange={(event => setValue({...value, street: event.target.value}))}/>
-                    </FormGroup>
-                </Col>
-                <Col>
-                    <FormGroup className={"mb-3"} controlId={"houseNumber"}>
-                        <FormLabel>House number</FormLabel>
-                        <FormControl type={"text"} placeholder={"1"} value={value.houseNumber} onChange={(event => setValue({...value, houseNumber: event.target.value}))}/>
-                    </FormGroup>
-                </Col>
-            </Row>
-            <FormGroup className={"mb-3"} controlId={"salary"}>
-                <FormLabel>Salary</FormLabel>
-                <FormControl type={"number"} placeholder={"10000"} value={value.salary} onChange={(event => setValue({...value, salary: event.target.value}))}/>
-            </FormGroup>
-            <FormGroup className={"mb-3"} controlId={"status"}>
-                <FormLabel>Status</FormLabel>
-                <Form.Select defaultValue={value.status} onChange={(event => setValue({...value, status: event.target.value}))}>
-                    <option unselectable={true}>Choose a status...</option>
-                    <option value={"ACTIVE"}>Active</option>
-                    <option value={"DELETED"}>Deleted</option>
-                    <option value={"HOLIDAY"}>Holiday</option>
-                    <option value={"SICK"}>Sick</option>
-                </Form.Select>
-            </FormGroup>
-            <FormGroup className={"mb-3"} controlId={"privilege"}>
-                <FormLabel>Privilege</FormLabel>
-                <Form.Select defaultValue={value.privilege} onChange={(event => setValue({...value, privilege: event.target.value}))}>
-                    <option unselectable={true}>Choose a privilege...</option>
-                    <option value={"USER"}>User</option>
-                    <option value={"SUPER_USER"}>Super user</option>
-                    <option value={"ADMIN"}>Admin</option>
-                </Form.Select>
-            </FormGroup>
-            <Button variant="primary" type="submit">
-                Submit
-            </Button>
-        </Form>
     )
 }
 
