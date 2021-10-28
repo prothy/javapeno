67df1743445aed26a6729a43a7d6fcfc816599b6
prothy <lukacsurgay@gmail.com>
2021-10-28 21:24:16 +0200

    feat (#34): change sidenav color to mango

--- src/components/Navigation/SideDrawer/SideDrawer.jsx
+++ src/components/Navigation/SideDrawer/SideDrawer.jsx
@@ -1,44 +1,49 @@
 import React from 'react';
 import {CashStack, HouseDoorFill, PersonLinesFill, PersonPlusFill} from "react-bootstrap-icons";
-import './SideDrawer.module.css';
-import {Nav, NavItem, NavLink} from "react-bootstrap";
+import {Nav, NavLink, Accordion } from "react-bootstrap";
 import {LinkContainer} from 'react-router-bootstrap'
+
+import './SideDrawer.css';
 
 const SideDrawer = () => {
   return(
       <Nav className="flex-column" variant={"pills"} >
-          <NavItem>
-              <LinkContainer to={"/home"}>
-                  <NavLink eventKey={"home"} >
-                      <HouseDoorFill />
-                      Home
-                  </NavLink>
-              </LinkContainer>
-          </NavItem>
-          <NavItem>
-              <LinkContainer to={"/employees"}>
-                  <NavLink eventKey={"employees"} >
-                      <PersonLinesFill />
-                      Employees
-                  </NavLink>
-              </LinkContainer>
-          </NavItem>
-          <NavItem className={"sub-item"}>
-              <LinkContainer to={"/create-employee"} >
-                  <NavLink eventKey={"create-employee"} >
-                      <PersonPlusFill />
-                      Create new user
-                  </NavLink>
-              </LinkContainer>
-          </NavItem>
-          <NavItem>
-              <LinkContainer to={"/transactions"}>
-                  <NavLink eventKey={"transactions"} >
-                      <CashStack />
-                      Transactions
-                  </NavLink>
-              </LinkContainer>
-          </NavItem>
+        <LinkContainer to={"/home"}>
+            <NavLink eventKey={"home"} >
+                <HouseDoorFill />
+                Home
+            </NavLink>
+        </LinkContainer>
+        <Accordion flush>
+            <Accordion.Item eventKey="0">
+                <Accordion.Header>Employees</Accordion.Header>
+                <Accordion.Body>
+                <LinkContainer to={"/employees"}>
+                    <NavLink eventKey={"employees"} >
+                        <PersonLinesFill />
+                        List all employees
+                    </NavLink>
+                </LinkContainer>
+                <LinkContainer to={"/create-employee"} >
+                    <NavLink eventKey={"create-employee"} >
+                        <PersonPlusFill />
+                        Add new employee
+                    </NavLink>
+                </LinkContainer>
+                </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="1">
+                <Accordion.Header>Transactions</Accordion.Header>
+                <Accordion.Body>
+                <LinkContainer to={"/transactions"}>
+                    <NavLink eventKey={"transactions"} >
+                        <CashStack />
+                        List all transactions
+                    </NavLink>
+                </LinkContainer>
+                </Accordion.Body>
+            </Accordion.Item>
+        </Accordion>
       </Nav>
 
 
