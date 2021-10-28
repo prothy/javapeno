2a64168cb0dff57ae1004a1c0695cad67220fa77
VepeP <57757745+VepeP@users.noreply.github.com>
2021-10-28 18:45:44 +0200

    refactor (33): refactor ChangePassword.jsx, added userId, getUserURL to dependency

--- src/components/Employee/Employee.jsx
+++ src/components/Employee/Employee.jsx
@@ -69,7 +69,7 @@ let Employee = () => {
                 setData(data);
             })
             .catch(err => console.error(err));
-    }, [fetchDataGetIncludeCors]);
+    }, [fetchDataGetIncludeCors, getUserURL, userId]);
 
     return (
         <div className="employee">
