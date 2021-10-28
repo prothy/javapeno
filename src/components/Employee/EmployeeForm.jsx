a8005f6ce0d71dc4837a43c012a6a40b7189515f
Zoltán Baranyay <zbaranyay@gmail.com>
2021-10-28 19:20:20 +0200

    feat (#11): add updateUserURL constants in the EmployeeForm.jsx component

--- src/components/Employee/EmployeeForm.jsx
+++ src/components/Employee/EmployeeForm.jsx
@@ -49,6 +49,7 @@ const EmployeeForm = (props, callback, deps) => {
     }
 
     const addUserURL = "http://localhost:8080/api/user/add";
+    const updateUserURL = "http://localhost:8080/api/user/update";
 
     const fetchEmployeeForm = useCallback(async (formattedValue) => {
         await fetchJsonDataPostIncludeCors(addUserURL, JSON.stringify(formattedValue))
