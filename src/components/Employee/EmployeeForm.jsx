e40f7a5d9fc5ca1d66a232de4f1f56178a3606cb
Zoltán Baranyay <zbaranyay@gmail.com>
2021-10-28 14:03:11 +0200

    feat (#11): user's data fill the EmployeeForm component if we edit the user

--- src/components/Employee/EmployeeForm.jsx
+++ src/components/Employee/EmployeeForm.jsx
@@ -1,21 +1,26 @@
-import React, {useCallback, useEffect, useState} from "react";
+import React, {useCallback, useState} from "react";
 import {Button, Col, Form, FormControl, FormGroup, FormLabel, Row} from "react-bootstrap";
-import {useHistory} from "react-router-dom";
+import {useHistory, useLocation} from "react-router-dom";
 
-const EmployeeForm = (callback, deps) => {
+const EmployeeForm = (props, callback, deps) => {
+    const location = useLocation();
+    const userData = location.state?.userData;
+    console.log(userData)
     const [value, setValue] = useState({
-        name: "",
-        phoneNumber: "",
-        email: "",
-        country: "Hungary",
-        city: "",
-        postalCode: "",
-        street: "",
-        houseNumber: "",
-        salary: 0,
-        status: "ACTIVE",
-        privilege: "USER"
-    })
+        id: userData?.id,
+        name: userData?.name,
+        phoneNumber: userData?.phoneNumber,
+        email: userData?.email,
+        country: userData?.address?.country,
+        city: userData?.address?.city,
+        postalCode: userData?.address?.postalCode,
+        street: userData?.address?.street,
+        houseNumber: userData?.address?.houseNumber,
+        salary: userData?.salary,
+        status: userData?.status,
+        privilege: userData?.privilege
+    });
+
     const history = useHistory();
 
     let formattedValue = {};
@@ -23,6 +28,7 @@ const EmployeeForm = (callback, deps) => {
     const onSubmitHandler = (event) => {
         event.preventDefault();
         formattedValue = {
+            id: value.id,
             name: value.name,
             phoneNumber: value.phoneNumber,
             email: value.email,
@@ -56,26 +62,27 @@ const EmployeeForm = (callback, deps) => {
         history.push("/employees")
 
     }, [history])
-    
+
     return (
         <Form onSubmitCapture={onSubmitHandler} style={{marginBottom: "100px"}}>
             <FormGroup className={"mb-3"} controlId={"name"} >
                 <FormLabel>Full name</FormLabel>
-                <FormControl type={"text"}  placeholder={"John Doe"} onChange={(event => setValue({...value, name: event.target.value}))} />
+                <FormControl type={"text"}  placeholder={"John Doe"} value={value.name} onChange={(event => setValue({...value, name: event.target.value}))} />
             </FormGroup>
             <FormGroup className={"mb-3"} controlId={"phoneNumber"} >
                 <FormLabel>Phone number</FormLabel>
-                <FormControl type={"tel"} placeholder={"+36101234567"} onChange={(event => setValue({...value, phoneNumber: event.target.value}))}/>
+                <FormControl type={"tel"} placeholder={"+36101234567"} value={value.phoneNumber} onChange={(event => setValue({...value, phoneNumber: event.target.value}))}/>
             </FormGroup>
             <FormGroup className={"mb-3"} controlId={"email"}>
                 <FormLabel>Email</FormLabel>
-                <FormControl type={"email"} placeholder={"john@doe.com"} onChange={(event => setValue({...value, email: event.target.value}))}/>
+                <FormControl type={"email"} placeholder={"john@doe.com"} value={value.email} onChange={(event => setValue({...value, email: event.target.value}))}/>
             </FormGroup>
             <Row>
                 <Col >
                     <FormGroup className={"mb-3"} controlId={"country"}>
                         <FormLabel>Country</FormLabel>
-                        <Form.Select onChange={(event => setValue({...value, country: event.target.value}))}>
+                        <Form.Select defaultValue={value.country} onChange={(event => setValue({...value, country: event.target.value}))}>
+                            <option unselectable={true}>Choose a country...</option>
                             <option>Hungary</option>
                             <option>Austria</option>
                             <option>Slovakia</option>
@@ -91,13 +98,13 @@ const EmployeeForm = (callback, deps) => {
                 <Col>
                     <FormGroup className={"mb-3"} controlId={"city"}>
                         <FormLabel>City</FormLabel>
-                        <FormControl type={"text"} placeholder={"Budapest"} onChange={(event => setValue({...value, city: event.target.value}))}/>
+                        <FormControl type={"text"} placeholder={"Budapest"} value={value.city} onChange={(event => setValue({...value, city: event.target.value}))}/>
                     </FormGroup>
                 </Col>
                 <Col>
                     <FormGroup className={"mb-3"} controlId={"postalCode"}>
                         <FormLabel>Zip</FormLabel>
-                        <FormControl type={"text"} placeholder={"1234"} onChange={(event => setValue({...value, postalCode: event.target.value}))}/>
+                        <FormControl type={"text"} placeholder={"1234"} value={value.postalCode} onChange={(event => setValue({...value, postalCode: event.target.value}))}/>
                     </FormGroup>
                 </Col>
             </Row>
@@ -105,23 +112,24 @@ const EmployeeForm = (callback, deps) => {
                 <Col xs={8}>
                     <FormGroup className={"mb-3"} controlId={"street"}>
                         <FormLabel>Address</FormLabel>
-                        <FormControl type={"text"} placeholder={"Example street"} onChange={(event => setValue({...value, street: event.target.value}))}/>
+                        <FormControl type={"text"} placeholder={"Example street"} value={value.street} onChange={(event => setValue({...value, street: event.target.value}))}/>
                     </FormGroup>
                 </Col>
                 <Col>
                     <FormGroup className={"mb-3"} controlId={"houseNumber"}>
                         <FormLabel>House number</FormLabel>
-                        <FormControl type={"text"} placeholder={"1"} onChange={(event => setValue({...value, houseNumber: event.target.value}))}/>
+                        <FormControl type={"text"} placeholder={"1"} value={value.houseNumber} onChange={(event => setValue({...value, houseNumber: event.target.value}))}/>
                     </FormGroup>
                 </Col>
             </Row>
             <FormGroup className={"mb-3"} controlId={"salary"}>
                 <FormLabel>Salary</FormLabel>
-                <FormControl type={"number"} placeholder={"10000"} onChange={(event => setValue({...value, salary: event.target.value}))}/>
+                <FormControl type={"number"} placeholder={"10000"} value={value.salary} onChange={(event => setValue({...value, salary: event.target.value}))}/>
             </FormGroup>
             <FormGroup className={"mb-3"} controlId={"status"}>
                 <FormLabel>Status</FormLabel>
-                <Form.Select onChange={(event => setValue({...value, status: event.target.value}))}>
+                <Form.Select defaultValue={value.status} onChange={(event => setValue({...value, status: event.target.value}))}>
+                    <option unselectable={true}>Choose a status...</option>
                     <option>ACTIVE</option>
                     <option>DELETED</option>
                     <option>HOLIDAY</option>
@@ -130,7 +138,8 @@ const EmployeeForm = (callback, deps) => {
             </FormGroup>
             <FormGroup className={"mb-3"} controlId={"privilege"}>
                 <FormLabel>Privilege</FormLabel>
-                <Form.Select onChange={(event => setValue({...value, privilege: event.target.value}))}>
+                <Form.Select defaultValue={value.privilege} onChange={(event => setValue({...value, privilege: event.target.value}))}>
+                    <option unselectable={true}>Choose a privilege...</option>
                     <option>USER</option>
                     <option>SUPER_USER</option>
                     <option>ADMIN</option>
@@ -141,6 +150,8 @@ const EmployeeForm = (callback, deps) => {
             </Button>
         </Form>
     )
+
+
 }
 
 export default EmployeeForm;
