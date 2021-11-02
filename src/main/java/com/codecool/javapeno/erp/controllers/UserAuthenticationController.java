ad31edc558bed38af5277a4bd67364a6059efdfe
Ichy91 <farago.istvan91@gmail.com>
2021-11-02 14:13:51 +0100

    feat (#48): add token refresh endpoint

--- src/main/java/com/codecool/javapeno/erp/controllers/UserAuthenticationController.java
+++ src/main/java/com/codecool/javapeno/erp/controllers/UserAuthenticationController.java
@@ -1,23 +1,80 @@
 package com.codecool.javapeno.erp.controllers;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.codecool.javapeno.erp.entities.User;
 import com.codecool.javapeno.erp.entities.UserAuthentication;
 import com.codecool.javapeno.erp.services.UserAuthenticationService;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.*;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.HttpStatus.FORBIDDEN;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 
 @RestController
 @RequestMapping("/api/user-authentication-service")
 @RequiredArgsConstructor
+@Slf4j
 public class UserAuthenticationController {
     private final UserAuthenticationService userAuthenticationService;
 
     @PostMapping(path = "/register")
     public void registerAuthentication(@RequestBody UserAuthentication userAuthentication) {
         userAuthenticationService.registerAuthentication(userAuthentication);
+    }
+
+    @GetMapping("/token/refresh")
+    public void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String authorizationHeader = request.getHeader(AUTHORIZATION);
+        if (authorizationHeader != null && authorizationHeader.startsWith("Javapeno ")) {
+            try {
+                String refresh_token = authorizationHeader.substring("Javapeno ".length());
+                Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
+                JWTVerifier verifier = JWT.require(algorithm).build();
+                DecodedJWT decodedJWT = verifier.verify(refresh_token);
+                String username = decodedJWT.getSubject();
+                UserAuthentication userAuthentication = userAuthenticationService.getAuthenticationByUsername(username);
+                User user = userAuthentication.getUser();
+
+                String access_token = JWT.create()
+                        .withSubject(userAuthentication.getUsername())
+                        .withExpiresAt(new Date(System.currentTimeMillis() + 10 * 60 * 1000))
+                        .withIssuer(request.getRequestURL().toString())
+                        .withClaim("privilege", new ArrayList<>(Collections.singleton(user.getPrivilege().toString())))
+                        .sign(algorithm);
+
+                Map<String, String> tokens = new HashMap<>();
+                tokens.put("access_token", access_token);
+                tokens.put("refresh_token", refresh_token);
+
+                response.setContentType(APPLICATION_JSON_VALUE);
+                new ObjectMapper().writeValue(response.getOutputStream(), tokens);
+
+            } catch (Exception exception) {
+                response.setHeader("error", exception.getMessage());
+                response.setStatus(FORBIDDEN.value());
+//                    response.sendError(FORBIDDEN.value());
+                Map<String, String> error = new HashMap<>();
+                error.put("error_message", exception.getMessage());
+
+                response.setContentType(APPLICATION_JSON_VALUE);
+                new ObjectMapper().writeValue(response.getOutputStream(), error);
+            }
+        } else {
+            throw new RuntimeException("Refresh token is missing");
+        }
     }
 }
