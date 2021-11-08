abb035f74b085499e3e157f064da69da5057aafb
Ichy91 <farago.istvan91@gmail.com>
2021-11-08 13:29:50 +0100

    feat (#48): add permission for the user-authentication-data endpoint

--- src/main/java/com/codecool/javapeno/erp/filter/CustomAuthorizationFilter.java
+++ src/main/java/com/codecool/javapeno/erp/filter/CustomAuthorizationFilter.java
@@ -28,7 +28,7 @@ public class CustomAuthorizationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         if (request.getServletPath().equals("/api/login") ||
-                request.getServletPath().equals("/api/user-authentication-service/token/refresh")) {
+                request.getServletPath().equals("/api/user-authentication-service/user-authentication-data")) {
             filterChain.doFilter(request, response);
         } else {
             String authorizationHeader = request.getHeader(AUTHORIZATION);
