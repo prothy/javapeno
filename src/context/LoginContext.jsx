81a3e67d2b555ae56507442f72b783427ad30177
prothy <lukacsurgay@gmail.com>
2021-11-09 14:33:04 +0100

    feat: add login context component

---
+++ src/context/LoginContext.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+export const UserContext = React.createContext('')
+
+const LoginContext = ({children}) => {
+    const [user, setUser] = useState('');
+
+    return (
+        <UserContext.Provider value={[user, setUser]}>
+            {children}
+        </UserContext.Provider>
+    );
+}
+
+export default LoginContext;
