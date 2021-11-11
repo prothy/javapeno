a520fc8ad9d9f3d3f3b16c9b836327b9003fad88
prothy <lukacsurgay@gmail.com>
2021-11-11 17:21:09 +0100

    feat: add modal when accessing unauthorized route

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,14 +1,22 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router';
+
+import LoginModal from '../Navigation/Login/LoginModal';
 
 const Javapeno = () => {
+    const [show, setShow] = useState(false)
+    const history = useHistory()
+
     useEffect(() => {
         if (new URLSearchParams(window.location.search).get("unauthorized") === '') {
-            console.log('doodo');
+            history.push("/")
+            setShow(true)
         } 
     })
     return (
         <article>
             <h2>Welcome to Javapeño's ERP System!</h2>
+            <LoginModal show={show} handleClose={() => setShow(false)}></LoginModal>
         </article>
     );
 };
